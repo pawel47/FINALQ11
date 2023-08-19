62fc6d2494fe9396d24352c693ae83c414836652
pawel47 <pawelwierzbicki@rocketmail.com>
2023-08-19 13:36:48 +0000

    Q11
    Signed-off-by: pawel47 <pawelwierzbicki@rocketmail.com>

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include "mbed.h"
+
+DigitalIn button1(PD_13);
+DigitalIn button2(PB_15);
+DigitalOut led1(PC_9);
+DigitalOut led2(PB_14);
+
+int main()
+{
+    while (1) {
+         if(button1){
+        led1 = 1;
+        led2 = 0;
+        } 
+        if(button2){
+            led1 = 0;
+            led2 = 1;
+        }
+        else {
+        led1 = 0;
+        led2 =0 ;
+
+        printf ("\r\n");
+
+    }
+        
+    }
+
+}
+
